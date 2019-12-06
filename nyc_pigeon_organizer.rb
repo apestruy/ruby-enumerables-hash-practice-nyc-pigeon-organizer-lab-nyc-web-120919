eab02bf1e2eb0ffd5c3e272df3b87daa3800e890
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-06 03:57:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,10 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
-  
+  data.each do |key, value|
+    value.each do |key_2, array|
+      array.each do |names|
+        pigeon_list[names] = {:color => [], :gender => [], :lives => []}
+      end 
+    end 
+  end 
 end
